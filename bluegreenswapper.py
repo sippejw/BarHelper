8cc2cfe789f19bea220dade1d4ff4df877cdee86
JacobJustice <jacobjustice@gmail.com>
2019-01-26 14:40:15 -0500

    Channel swapper script and channel swapped images

--- bluegreenswapper.py
+++ bluegreenswapper.py
@@ -2,9 +2,17 @@ from PIL import Image
 import glob, os
 import numpy as np
 counter = 0
-for infile in glob.glob('./images/*'):
+for infile in glob.glob('./images/*/*'):
     print(infile)
     image = Image.open(infile)
     counter = counter + 1
-    flip_image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    flip_image.save('./flipped-images/flipped' + str(counter) + '.jpg')
+    imarray = np.asarray(Image.open(infile))
+    imarray.setflags(write=1)
+    for row in imarray:
+	print(row)
+	for pixel in row:
+            pixel[1], pixel[2] = pixel[2], pixel[1]
+    imdifFace = Image.fromarray(imarray)
+    imdifFace.save('./images/channel-swapped/chswapped' + str(counter) + '.jpg')
+
+            
