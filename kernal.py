01ebfcc148ab754efac36412f6f3c93a6d86071a
JacobJustice <jacobjustice@gmail.com>
2019-01-27 05:21:23 -0500

    new main

--- kernal.py
+++ kernal.py
@@ -4,9 +4,38 @@ from scipy import misc
 import matplotlib.pyplot as plt
 from PIL import Image
 
-image = np.asarray(Image.open('./images/raw/image.jpg').convert('L'))
+#image = np.asarray(Image.open('./images/raw/image.jpg').convert('L'))
+image = './images/raw/image.jpg'
+def colConvolude(image,  thresh):
 
-def convolude(image, kernal):
+    kernal = np.array([[1, 1, 0, -1, -1],   #5x5 kernel
+    [1, 2, 0, -2, -1],
+    [2, 3, 0, -3, -2],
+    [1, 2, 0, -2, -1],
+    [1, 1, 0, -1, -1]])
+
+    if len(image)>1024 or len(image[0])>512:
+        image = resize(image)
+
+    (dim_x, dim_y) = np.shape(image)
+    newImage= np.zeros(image.shape) #the new image. the same size as the image I will filter
+    for i in range(1,dim_x-1): #the range starts from 1 to avoid the column and row of zeros, and ends before the last col and row of zeros
+        for j in range(1,dim_y-1):
+            newImage.setflags(write=1)
+            imageEntry= image[i-2:i+3, j-2:j+3]
+          #  print("image entry: " + str(imageEntry))
+          #  print("kernal: " + str(kernal))
+            if len(imageEntry) == 5 and len(imageEntry[0]) == 5:
+                valor = np.sum(imageEntry*kernal)    #Matrix 3x3 is filled with the elements around each [i, j] entry of the array
+                newImage[i, j] = valor
+
+        newImage = threshold(newImage, thresh)
+    return newImage
+
+def rowConvolude(image, thresh):
+    kernal = np.array([[-1, -2, -1],   #3x3 kernel
+    [0, 0, 0],
+    [1,2,1]])
     if len(image)>1024 or len(image[0])>512:
         image = resize(image)
 
@@ -22,8 +51,24 @@ def convolude(image, kernal):
                 valor = np.sum(imageEntry*kernal)    #Matrix 3x3 is filled with the elements around each [i, j] entry of the array
                 newImage[i, j] = valor
 
-        newImage = threshold(newImage, 128)
+        newImage = threshold(newImage, thresh)
     return newImage
+
+def colSum(image):
+    transImage = np.transpose(image)
+    (dim_x, dim_y) = np.shape(transImage)
+    print(dim_x, dim_y)
+    kernal = [[1] * dim_y, [2] * dim_y, [1] * dim_y] # for some reason y is the x dim
+    sumVector= np.zeros(dim_y) #the new image. the same size as the image I will filter
+
+    for i in range(1,dim_x-1): #the range starts from 1 to avoid the column and row of zeros, and ends before the last col and row of zeros
+        sumVector.setflags(write=1)
+        imageEntry= transImage[i-1:i+2, 0:dim_y]
+
+        valor = np.sum(imageEntry*kernal)    #Matrix 3x3 is filled with the elements around each [i, j] entry of the array
+        sumVector[i] = valor
+
+    return sumVector
 
 def rowSum(image):
     (dim_x, dim_y) = np.shape(image)
@@ -55,7 +100,7 @@ def threshold(image, thresh):
                 image[i][j] = 0
     return image
 
-def rankRows(vector, n):
+def rankRow(vector, n):
     max = [-1] * n
     for i in range(len(vector)):
         added = False
@@ -67,25 +112,52 @@ def rankRows(vector, n):
             j += 1
     return max
 
+def rankCol(vector, n):
+    max = [-1] * n
+    for i in range(len(vector)):
+        added = False
+        j = 0
+        while not added and j < n:
+            if vector[max[j]] < vector[i] and (max[j] not in range(i - 10, i + 10) or max[j] < 0 and i < 2):
+                added = True
+                max[j] = i
+            j += 1
+    return max
+    
+def main(infile):
+    image= np.asarray(Image.open(infile).convert('L'))
+    rowImage= rowConvolude(image, 128)
+    colImage= colConvolude(image, 80)
 
-ker1 = np.array([[-1, -2, -1],   #3x3 kernel
-[0, 0, 0],
-[1,2,1]])
+    rows = rowSum(rowImage)
+    rows = rankRow(rows,5)
+    cols = colSum(colImage)
+    cols = rankCol(cols, 5)
+    
+    return rows[2]/len(image)    
+    
+#rowImage= rowConvolude(image, 128)
+#colImage= colConvolude(image, 80)
+##testim= np.asarray(Image.open('./test.jpg').convert('L'))
+#
+#rows = rowSum(rowImage)
+#rows = rankRow(rows, 5)
+#cols = colSum(colImage)
+#cols = rankCol(cols, 5)
+#
+#newtest = np.asarray(Image.fromarray(colImage.copy()).convert('L')).copy()
+#newtest.setflags(write=1)
+#for i in rows:
+#    for pix in range(len(newtest[i])):
+#        newtest[i][pix] = 255
+#
+#for i in cols:
+#    for pix in range(len(newtest)):
+#        newtest[pix][i] = 255
+#Image.fromarray(newtest).save('newtest.jpg')
 
-#newImage= convolude(image, ker1)
-testim= np.asarray(Image.open('./test.jpg').convert('L'))
+print(main(image))
 
-rows = rowSum(testim)
-rows = rankRows(rows, 5)
-
-newtest = testim.copy()
-newtest.setflags(write=1)
-for i in rows:
-    for pix in range(len(newtest[i])):
-        newtest[i][pix] = 255
-Image.fromarray(newtest).save('newtest.jpg')
-
-print(rows)
 #im = Image.fromarray(newImage).convert('L')
 #im.save('./test.jpg')
 
